2dc379583bd872443159810d14804a1151c84817
Josh Peak <neozenith.dev@gmail.com>
2024-01-18 11:46:49 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# HEIC to PNG/JPG
+
+## Quickstart
+
+### Setup
+
+```sh
+python3 -m venv .venv
+.venv/bin/python3 -m pip install -U pip pip-tools
+.venv/bin/python3 -m piptools compile --generate-hashes requirements.in --output-file requirements.txt
+.venv/bin/python3 -m pip install -r requirements.txt --require-hashes --no-deps --only-binary :all:
+```
+
+### Run
+
+
+```sh
+.venv/bin/python3 convert.py
+```
---
+++ convert.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from pathlib import Path
+
+def discover_heic(start_dir: Path = Path("./data/HEIC/")):
+    """Recursively walk and discover a list of HEIC files to process."""
+    return [ start_dir /f for f in os.listdir(start_dir) if f.lower().endswith(".heic")]
+
+def targeter(p: Path, suffix: str = "JPEG"):
+    return (p.parent.parent / suffix / p.name).with_suffix(f".{suffix}")
+
+def process_image(img_path: Path, type: str = "PNG"):
+    """Given the path to a HEIC, convert it to a PNG."""
+    from PIL import Image
+    from pillow_heif import register_heif_opener
+
+    register_heif_opener()
+    target_path = targeter(img_path, type)
+    image = Image.open(img_path)
+    image.save(target_path,type)
+    return target_path
+
+
+def main(args):
+    """Process HEIC images to PNG/JPG."""
+    for f in discover_heic():
+        print(f"Processing {f} --> ")
+        print(process_image(f, "JPEG"))
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
