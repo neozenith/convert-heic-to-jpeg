64af86b82272223814971a120473814e6aee35d9
Josh Peak <neozenith.dev@gmail.com>
2024-03-05 14:51:21 +1100

    async process documents through texttract

--- textract.py
+++ textract.py
@@ -1,6 +1,9 @@
+from typing import List
 import boto3
 import sys
+import json
 from pathlib import Path
+import base64
 import os
 from pprint import pprint as pp
 from dotenv import load_dotenv
@@ -23,8 +26,53 @@ comprehend_client = session.client("comprehend")
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/client/analyze_expense.html
 # https://docs.aws.amazon.com/textract/latest/dg/what-is.html
 
+# {
+#    "Document": { 
+#       "Bytes": blob,
+#       "S3Object": { 
+#          "Bucket": "string",
+#          "Name": "string",
+#          "Version": "string"
+#       }
+#    },
+#    "FeatureTypes": [ "string" ],
+#    "HumanLoopConfig": { 
+#       "DataAttributes": { 
+#          "ContentClassifiers": [ "string" ]
+#       },
+#       "FlowDefinitionArn": "string",
+#       "HumanLoopName": "string"
+#    }
+# }
+
+def filename_filter(f):
+    return any(f.name.lower().endswith(x) for x in ["jpg", "jpeg"])
+
+async def list_files():
+    return [f for f in Path("./data/JPEG/").iterdir() if filename_filter(f)]
+
+async def process_document(f, overwrite=False):
+    document_bytes = f.read_bytes()
+    print(f)
+    outputfilename: Path = f.parent / (f.name + '.json')
+    print(outputfilename)
+    if overwrite or not outputfilename.exists():
+        response = textract_client.analyze_expense(    
+            Document={
+                'Bytes': document_bytes,
+            }
+        )
+        outputfilename.write_text(json.dumps(response, indent=2))
+    else:
+        print(f"Skipping because {overwrite=} or {outputfilename.exists()=}")
+    return outputfilename
+
 async def main(args):
-    ...
+    files: List[Path] = await list_files()
+    print(files)
+    results = await asyncio.gather(*[process_document(f) for f in files])
+    print(results)
+
 
 
 
